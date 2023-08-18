dd4ad7338479434d04b2d9fb2019ceb00a8e6974
Yash Budhia <142312760+yashbudhia@users.noreply.github.com>
2023-08-18 12:10:22 +0530

    Update palindrome.js

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -5,9 +5,25 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-palindrome`
 */
+function isAnagram(str1, str2) {
+    let S1 = str1.split('');
+    let S2 = str2.split('');
+    if (S1.length !== S2.length)
+    { return false ; }
+    else {
+        let n = S1.length ;
+        for ( i = 0; i < n ; i++)
+        {
+           for ( j= 0; j <n ; j++)
+           {
+                if ( S1[i] == S2[j])
+               { return true;
+               }
+               break;
+            }
 
-function isPalindrome(str) {
-  return true;
+    }
+    }
 }
 
 module.exports = isPalindrome;
